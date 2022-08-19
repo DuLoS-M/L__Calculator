e46cba71b64d5b49e9f9be611d4cdfde05ae5492
DuLoS-M <e_craud@hotmail.com>
2022-08-19 08:27:40 -0600

    Created basic functions of basic operations

---
+++ script.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function substraction(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function power(a, b) {
+    return a**b;
+}
+
